42dddbb7dd78a84940f9c88f4323e116ff5aa517
Bill Calkins <bill@calkinsconsulting.com>
2021-05-15 16:08:46 -0500

    Initial Commit

---
+++ CreditCardFormatter/CreditCardFormatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardFormatterApp.swift
+//  CreditCardFormatter
+//
+//  Created by William Calkins on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardFormatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
